ed4fe6c58897cbaed138c7984407179b4ff3496b
Keren DONG <keren.dong@kungfu.link>
2021-06-28 17:49:40 +0800

    Set default branch for detached HEAD

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const core = require('@actions/core');
 const github = require("@actions/github");
-const git = require('git-client');
 const lib = require("./lib.js");
 
 try {
     const context = github.context;
     const bumpKeyword = core.getInput('bump-keyword');
+    const defaultBranch = core.getInput('default-branch');
 
     console.log(`GitHub Actor: ${context.actor}`);
 
     const setupGit = async function () {
-        await git("config", "--global", "user.name", context.actor);
-        await git("config", "--global", "user.email", `${context.actor}@noreply.kungfu.link`);
+        await lib.gitRun("config", "--global", "user.name", context.actor);
+        await lib.gitRun("config", "--global", "user.email", `${context.actor}@noreply.kungfu.link`);
     };
 
-    setupGit().then(() => lib.bumpVersion(bumpKeyword));
+    setupGit().then(() => lib.bumpVersion(bumpKeyword, defaultBranch));
 } catch (error) {
     core.setFailed(error.message);
 }
