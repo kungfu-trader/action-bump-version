3e3fc3247b210bea05884578a2bc7eecdbf24f1b
Keren DONG <keren.dong@kungfu.link>
2021-06-28 13:30:32 +0800

    Update actions

--- dist/index.js
+++ dist/index.js
@@ -32,7 +32,7 @@ const { spawn, spawnSync } = __nccwpck_require__(3129);
 const spawnOptsInherit = { shell: true, stdio: "inherit", windowsHide: true };
 const spawnOptsPipe = { shell: true, stdio: "pipe", windowsHide: true };
 
-function getPackageJson(cwd = process.cwd()) {
+function getPackageJson(cwd) {
   const packageJsonPath = path.join(cwd, "package.json");
   if (!fs.existsSync(packageJsonPath)) {
     console.error("package.json not found");
@@ -40,41 +40,47 @@ function getPackageJson(cwd = process.cwd()) {
   return JSON.parse(fs.readFileSync(packageJsonPath));
 }
 
-function hasLerna(cwd = process.cwd()) {
+function hasLerna(cwd) {
   return fs.existsSync(path.join(cwd, "lerna.json"));
 }
 
-function getCurrentVersion(cwd = process.cwd()) {
+function getCurrentVersion(cwd) {
   const configPath = path.join(cwd, hasLerna(cwd) ? "lerna.json" : "package.json");
   const config = JSON.parse(fs.readFileSync(configPath));
   return semver.parse(config.version);
 }
 
-function bump(cwd = process.cwd()) {
+function bump(cwd, next) {
   if (hasLerna(cwd)) {
-    spawnSync("lerna", ["version", `pre${next}`, "--yes", "--no-git-tag-version", "--no-push"], spawnOptsInherit);
+    spawnSync("lerna", ["version", `${next}`, "--yes", "--no-git-tag-version", "--no-push"], spawnOptsInherit);
   } else {
-    spawnSync("yarn", ["version", `pre${next}`], spawnOptsInherit);
+    spawnSync("yarn", ["version", `${next}`], spawnOptsInherit);
   }
 }
 
-async function bumpToNext(cwd = process.cwd(), next = "minor") {
-  const currentVersion = getCurrentVersion();
+async function prepareNewBranch(cwd, next) {
+  const currentVersion = getCurrentVersion(cwd);
   console.log(`increment ${next}: ${currentVersion}`);
   const nextVersion = currentVersion.inc(next);
   const versionBranch = `dev/v${nextVersion.major}/v${nextVersion.major}.${nextVersion.minor}`;
   await git("switch", "-c", versionBranch);
-  bump(cwd);
+  await git("push", "-u", "origin", versionBranch);
 }
 
 const actions = {
-  "major": async (cwd = process.cwd()) => bumpToNext(cwd, "major"),
-  "minor": async (cwd = process.cwd()) => bumpToNext(cwd, "minor"),
-  "patch": (cwd = process.cwd()) => bump(cwd)
+  "premajor": (cwd) => {
+    prepareNewBranch(cwd, "premajor").then(() => bump(cwd, "premajor"));
+  },
+  "preminor": (cwd) => {
+    prepareNewBranch(cwd, "preminor").then(() => bump(cwd, "preminor"));
+  },
+  "prepatch": (cwd) => {
+    bump(cwd, "prepatch");
+  }
 };
 
-exports.bumpVersion = async function (bumpKeyword) {
-  actions[bumpKeyword]();
+exports.bumpVersion = function (bumpKeyword) {
+  actions[bumpKeyword](process.cwd());
 };
 
 /***/ }),
