ea509672c17183cc0ead2be04f039bd779b890eb
Keren DONG <keren.dong@kungfu.link>
2021-06-29 22:43:37 +0800

    Merge alpha to dev

--- lib.js
+++ lib.js
@@ -43,7 +43,6 @@ async function bump(cwd, keyword, branchPrefixes = [], pushMatch = true) {
   await gitCall("tag", `v${currentVersion.major}`);
   await gitCall("tag", `v${currentVersion.major}.${currentVersion.minor}`);
   await gitCall("push", "-f", "--tags");
-  await gitCall("fetch");
 
   if (pushMatch) {
     await gitCall("push");
@@ -60,7 +59,7 @@ async function bump(cwd, keyword, branchPrefixes = [], pushMatch = true) {
     const targetBranch = `${branchPrefix}/${branchPath}`;
     await gitCall("switch", targetBranch);
     await gitCall("merge", `origin/${upstreamBranch}`);
-    await gitCall("push", `HEAD:origin/${targetBranch}`);
+    await gitCall("push");
   }
 }
 
