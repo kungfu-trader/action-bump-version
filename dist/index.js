1b2fca3b1696f786166ff5894eabb3c17099e6e7
Keren DONG <keren.dong@kungfu.link>
2021-07-06 10:44:23 +0800

    Fix setup

--- dist/index.js
+++ dist/index.js
@@ -15,6 +15,7 @@ const handleError = (error) => {
 };
 
 const setup = exports.setup = async function (argv) {
+    const context = github.context;
     const octokit = github.getOctokit(argv.token);
     if (context.eventName == "pull_request") {
         const { data: pullRequest } = await octokit.rest.pulls.get({
