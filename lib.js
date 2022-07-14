6232818145bfd6ff4b32fed488bafc2dcc1ef29b
xinxinxiangying258 <923058707@qq.com>
2022-07-14 11:58:09 +0800

    variable names to use

--- lib.js
+++ lib.js
@@ -392,7 +392,7 @@ async function resetDefaultBranch(argv){ //更改默认分支名
   const octokit = github.getOctokit(argv.token);
   const { lastDevVersion } = await octokit.graphql( //获取最新版本
     `query {
-      repositories(name: "${argv.repo}", owner: "${argv.owner}") {
+      repository(name: "${argv.repo}", owner: "${argv.owner}") {
           refs(refPrefix: "refs/heads/dev/", last: 1){
             edges{
               node{
@@ -404,7 +404,7 @@ async function resetDefaultBranch(argv){ //更改默认分支名
       }
     }`,
   );  
-  const lastDevName = "dev/"+lastDevVersion.repositories.refs.edges.node.name; //给最新的版本号加上前缀
+  const lastDevName = "dev/"+lastDevVersion.repository.refs.edges.node.name; //给最新的版本号加上前缀
   await octokit.request('PATCH /repos/{owner}/{repo}', { //使用REST API来上传以更新默认分支名
     owner: argv.owner,
     repo: argv.repo,
