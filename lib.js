816a1ee1b85eb5b30c771988337ee3cb11a53ee6
xinxinxiangying258 <923058707@qq.com>
2022-07-14 14:19:00 +0800

    retyr what has been tested before

--- lib.js
+++ lib.js
@@ -390,7 +390,7 @@ async function mergeCall(argv, keyword) {
 }
 async function resetDefaultBranch(argv){ //更改默认分支名
   const octokit = github.getOctokit(argv.token);
-  const lastDevVersion  = await octokit.graphql( //获取最新版本
+  const lastDevVersion  = await octokit.graphql( 
     `query {
       repository(name: "${argv.repo}", owner: "${argv.owner}") {
           refs(refPrefix: "refs/heads/dev/", last: 1){
@@ -401,13 +401,13 @@ async function resetDefaultBranch(argv){ //更改默认分支名
         }
       }
     }`,
-  );  
+  );  //获取最新版本
   const lastDevName = "dev/"+lastDevVersion.repository.refs.nodes.name; //给最新的版本号加上前缀
-  await octokit.request('PATCH /repos/{owner}/{repo}', { //使用REST API来上传以更新默认分支名
+  const response = await octokit.request('PATCH /repos/{owner}/{repo}', { 
     owner: argv.owner,
     repo: argv.repo,
     default_branch: lastDevName
-  });
+  });//使用REST API来上传以更新默认分支名
   }
 
 exports.getChannel = getChannel;
