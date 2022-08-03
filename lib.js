053deb18a4907d5441f1c6ebb8df7101d67aef49
xinxinxiangying258 <923058707@qq.com>
2022-08-03 12:16:17 +0800

    passing string parameter whether need quotation marks

--- lib.js
+++ lib.js
@@ -423,11 +423,12 @@ async function* traversalPackagesGraphQL(octokit) {
   let hasNextPage = false; //let是可变变量,是否有下一页，用以判断是否要继续循环
   const maxPerPage = 100; //const是常量，每页最大值，这里定义为100，默认为30
   let startCursor = ''; //因为后续这里肯定是string类型的，所以这里先给它初始化为“”，注意不能初始化为=null，有风险
+
   do {
     const graphResponse = await octokit.graphql(`
       query{
         organization(login: "kungfu-trader") {
-          packages(first: ${maxPerPage}, after: ${startCursor}) {
+          packages(first: ${maxPerPage}, after: "${startCursor}") {
             totalCount
             pageInfo {
               hasNextPage
