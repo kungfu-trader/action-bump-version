88d070b08441c636e863f58c66013433e32f56fc
xinxinxiangying258 <923058707@qq.com>
2022-08-08 17:44:42 +0800

    update stringify ways of traversalResult

--- dist/index.js
+++ dist/index.js
@@ -755,9 +755,10 @@ const request = __nccwpck_require__(8699);
 exports.sendMessageToAirtable = async function (traversalResult) {
   //const messageToAirtable = JSON.stringify(traversalResult);
   console.log(typeof traversalResult);
-  //const param = '"' + `${traversalResult}` + '"';
+  const param = '"' + `${traversalResult}` + '"';
+  //const param = '"' + traversalResult + '"';
   //const param = traversalResult + ""; //要注意yarn build后会变为‘’
-  const param = JSON.stringify(traversalResult); //string化
+  //const param = JSON.stringify(traversalResult); //string化
   console.log(typeof param);
   //console.log(traversalResult);
   let stringBodyStore = {
