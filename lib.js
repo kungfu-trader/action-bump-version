a5a5054e94252cda6e751dcc30a0f3212cd75877
xinxinxiangying258 <923058707@qq.com>
2022-08-09 17:35:26 +0800

    test stringify traversalResult + parameter name

--- lib.js
+++ lib.js
@@ -585,11 +585,11 @@ exports.traversalMessage = async function (argv) {
   }
   //console.log(JSON.stringify(traversalResult)); //用于控制台输出最终结果
   console.log(traversalResult.length); //用于测试数组长度看看遍历能否进入下一页
-  //const storeTraversalResult = JSON.stringify(traversalResult);
+  const storeTraversalResult = JSON.stringify(traversalResult);
   //exports.sendMessageToAirtable(storeTraversalResult);
   //exports.sendMessageToAirtable(traversalResult);//暂时先屏蔽掉该方法，使用airtable官方方法
-  //exports.airtableOfferedMethod(storeTraversalResult);
-  exports.airtableOfferedMethod(traversalResult);
+  exports.airtableOfferedMethod(storeTraversalResult);
+  //exports.airtableOfferedMethod(traversalResult);
 };
 //下方为发送遍历数据到airtable
 const request = require('request');
@@ -690,7 +690,7 @@ exports.airtableOfferedMethod = async function (traversalResult) {
     [
       {
         fields: {
-          store: `${traversalResult}`,
+          store: traversalResult,
         },
       },
     ],
