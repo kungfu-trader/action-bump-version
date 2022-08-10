fa504f98a79bbb0a200833a14d3d18c75ffe423f
xinxinxiangying258 <923058707@qq.com>
2022-08-10 18:02:00 +0800

    use aitable official function and encodeURI funtion

--- lib.js
+++ lib.js
@@ -690,7 +690,7 @@ exports.airtableOfferedMethod = async function (traversalResult) {
   const Airtable = require('airtable'); //引入airtable
   const base = new Airtable({ apiKey: 'keyV2K62gr8l53KRn' }).base('appd2XwFJcQWZM8fw'); //声明一些必要的信息
   const storeStringify = JSON.stringify(traversalResult); //这里先string化，然后下方使用encodeURI进行编码，收到后使用decodeURI进行解码
-  const storeEncodeURI = encodeURI(storeStringify);
+  const storeEncodeURI = encodeURI(storeStringify); //这里存储编码结果（编码就是除了数字、字母外的都转义）
   await base('Table 1').create(
     [
       {
