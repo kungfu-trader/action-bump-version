1a7e728e18daacd1abb579cee3e96553ba164dd7
Keren DONG <keren.dong@kungfu.link>
2022-08-13 10:28:50 +0800

    [fix] prepare dev after patch

--- dist/index.js
+++ dist/index.js
@@ -552,16 +552,8 @@ async function mergeCall(argv, keyword) {
   if (keyword === 'patch') {
     // Prepare new prerelease version for dev channel
     const devChannel = `dev/${versionRef}`;
-    const alphaChannel = `origin/alpha/${versionRef}`;
     await gitCall('fetch');
     await gitCall('switch', '-c', devChannel, `origin/${devChannel}`);
-    await bumpCall(argv, 'patch', 'auto', false);
-    await gitCall('commit', '-a', '-m', `Update ${devChannel} to catch up alpha channel`);
-    await gitCall('merge', alphaChannel).catch((e) => {
-      console.error(e);
-    }); // merge alpha into dev
-    await gitCall('reset', '--hard'); // abandon failed merge
-    await gitCall('update-ref', '-d', 'MERGE_HEAD'); // and clean up MERGE_HEAD
     await bumpCall(argv, 'prepatch', 'auto', false);
     await gitCall('commit', '-a', '-m', `Update ${devChannel} to work on ${nextVersion}`);
     await gitCall('push', 'origin', `HEAD:${devChannel}`);
